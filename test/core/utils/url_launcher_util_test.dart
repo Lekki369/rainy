c0749bc1d05d3a09ec64e3001ecbefb1dbd575fe
Lekki369 <awomeweolamilekan@gmail.com>
2026-03-25 08:36:50 +0100

    add tests for string utils

---
+++ test/core/utils/url_launcher_util_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:rainy/core/utils/url_launcher_util.dart';
+
+import '../../helpers/fake_url_launcher.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    installFakeUrlLauncher();
+  });
+
+  group('openUrl', () {
+    test('opens valid https url', () async {
+      await expectLater(
+        openUrl('https://openstreetmap.org/copyright'),
+        completes,
+      );
+    });
+
+    test('throws when launch fails', () async {
+      installFakeUrlLauncher(launchResult: false);
+
+      await expectLater(
+        openUrl('https://example.com'),
+        throwsA(isA<Exception>()),
+      );
+    });
+  });
+}
